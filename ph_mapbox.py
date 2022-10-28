91ccd7fa0d70257b472c1119f1319e76aa750cc2
Nancy <nansimalgapo@gmail.com>
2022-10-28 15:33:41 +0800

    Update ph_mapbox.py

--- ph_mapbox.py
+++ ph_mapbox.py
@@ -6,9 +6,9 @@ import plotly.offline
 
 
 def plot_ph_cities():
-    ph_cities = pd.read_csv("files/ph.csv")
+    ph_cities = pd.read_csv("ph.csv")
     fig = px.scatter_mapbox(ph_cities, lat="lat", lon="lng", hover_name="city", hover_data=["population"],
                             color_discrete_sequence=["fuchsia"], zoom=5, height=800)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    plotly.offline.plot(fig, filename="templates/map_chart.html", auto_open=False)
+    plotly.offline.plot(fig, filename="map_chart.html", auto_open=False)
